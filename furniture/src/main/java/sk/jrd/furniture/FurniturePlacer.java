e873808e6e6a06ffd03b3ef78a91eac716b5358d
developing-jr <developing.jr@gmail.com>
2018-09-05 23:22:13 +0200

    logback setup

--- furniture/src/main/java/sk/jrd/furniture/FurniturePlacer.java
+++ furniture/src/main/java/sk/jrd/furniture/FurniturePlacer.java
@@ -1,10 +1,16 @@
 package sk.jrd.furniture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by DEVELOPING-JR.
  */
 public class FurniturePlacer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FurniturePlacer.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Start of Furniture Placer");
     }
 }
