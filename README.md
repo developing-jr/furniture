2a384a5ff0e544c6ec6847cd8fe7d1067f2189c0
developing-jr <developing.jr@gmail.com>
2018-09-06 00:34:47 +0200

    launcher for idea and command line with reading of sample system properties for room and furniture definitions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Room Furniture
-Optimal placement of room furniture
+Optimal placement of room furniture.
+
+Mandatory java system properties for start with definition of room and furniture:
+-DroomDefinition="5,6 ..###. .####. ###### ###### ...###" -DfurnitureDefinitions="A2#### B3.#.###.#."
+
+To start successfully created Room Furniture from terminal use following command line:
+java -DroomDefinition="5,6 ..###. .####. ###### ###### ...###" -DfurnitureDefinitions="A2#### B3.#.###.#." -jar furniture-1.0-SNAPSHOT-shaded.jar
--- furniture/src/main/java/sk/jrd/furniture/FurniturePlacer.java
+++ furniture/src/main/java/sk/jrd/furniture/FurniturePlacer.java
@@ -10,7 +10,12 @@ public class FurniturePlacer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FurniturePlacer.class);
 
+    private static final String ROOM_DEFINITION = System.getProperty("roomDefinition");
+    private static final String FURNITURE_DEFINITIONS = System.getProperty("furnitureDefinitions");
+
     public static void main(String[] args) {
         LOGGER.info("Start of Furniture Placer");
+        LOGGER.info("Room definition: {}", ROOM_DEFINITION);
+        LOGGER.info("Furniture definitions: {}", FURNITURE_DEFINITIONS);
     }
 }
